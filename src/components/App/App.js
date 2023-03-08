239a6e167a48371b3b8b2e330f28d7741cef723f
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-03-08 21:20:20 +0300

    init: start project

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import cl from './App.module.scss';
+
+const App = () => {
+	return (
+		<React.Fragment>
+			<div className={cl.container}>Registration form</div>
+		</React.Fragment>
+	);
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    extensions: ['jsx', '.js'],
+  },
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/',
+    assetModuleFilename: (pathData) => {
+      const filepath = path.dirname(pathData.filename).split('/').slice(1).join('/');
+      return `${filepath}/[name][hash][ext]`;
+    },
+  },
+	devServer: {
+    port: 3000,
+    open: true,
+    hot: false,
+    liveReload: true,
+    historyApiFallback: true,
+  },
+	devtool: 'source-map',
+  plugins: [
+    new HTMLWebpackPlugin({ template: './src/index.html', favicon: './src/assets/favicon.svg' }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name][contenthash].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: './src/static', to: './', noErrorOnMissing: true }],
+    }),
+    new ESLintPlugin({ extensions: ['js'] }),
+    new webpack.DefinePlugin({ 'process.env.PUBLIC_URL': JSON.stringify(process.env.PUBLIC_URL) }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+			{   
+				test: /\.svg$/, 
+				use: 'svg-inline-loader' 
+			},
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg|mp4)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
