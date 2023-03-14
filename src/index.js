56f118d9a1c543e974a7d5491041eb3e6918b5b8
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-03-14 18:13:59 +0300

    refactor: remove unnecessary packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-import { BrowserRouter } from 'react-router-dom';
 import './reset.scss';
 import { AlertProvider } from './components/contexts/AlertContext';
 import AlertPopup from './components/AlertPopup/AlertPopup';
@@ -10,10 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<AlertProvider>
-			<BrowserRouter basename={process.env.PUBLIC_URL}>
-				<App />
-				<AlertPopup />
-			</BrowserRouter>
+			<App />
+			<AlertPopup />
 		</AlertProvider>
 	</React.StrictMode>
 );
